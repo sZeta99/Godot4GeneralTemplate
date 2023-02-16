49451c41c495ae745defebbea06a0b7b90ca3f99
sZeta99 <the.black.mage.noir@gmail.com>
2023-02-16 16:20:49 +0100

    Risoluzione inserita, e fatti alcuni particles

--- GeneralTemplate/TemplateAssets/Scripts/UI/GlobalSettings.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-
-func toggle_fullscreen(value):
-	ProjectSettings.get_setting("player/look_sensitivity")
--- GeneralTemplate/TemplateAssets/Scripts/UI/SettingMenu.gd
+++ GeneralTemplate/TemplateAssets/Scripts/UI/SettingMenu.gd
@@ -1,14 +1,25 @@
 extends PopupPanel
 
-@onready var FULLSCREEN = %FullScreenButton
+
+const DISPLAY_MODE: Dictionary = {0 : DisplayServer.WINDOW_MODE_FULLSCREEN,
+1 : DisplayServer.WINDOW_MODE_WINDOWED
+}
+
+const RESOLUTION_SIZE: Dictionary = {0 : Vector2(720,576),
+1 : Vector2(1280, 720),
+2 : Vector2(1920,1080)
+}
 
 #Probabilmente un bug ma gli index non sono ricalibrabili da fuori
 func _on_full_screen_button_item_selected(index):
 
-	if index == 0 :
-		index = DisplayServer.WINDOW_MODE_FULLSCREEN
-	else:
-		index = DisplayServer.WINDOW_MODE_WINDOWED
-	DisplayServer.window_set_mode(index)
-	print_debug("Full Screen mode set to " + str(index))
+	DisplayServer.window_set_mode(DISPLAY_MODE.get(index))
+	print_debug("Full Screen mode set to " + str(DISPLAY_MODE.get(index)))
 
+
+
+func _on_resolution_button_item_selected(index):
+	print_debug(index)
+	DisplayServer.window_set_size(RESOLUTION_SIZE.get(index))
+	print_debug("Resolution Set to " + str(RESOLUTION_SIZE.get(index)))
+	
