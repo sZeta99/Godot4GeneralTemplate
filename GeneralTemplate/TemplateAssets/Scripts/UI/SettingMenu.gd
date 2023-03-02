f8996be576903836f5cd3e2b85ddc3afc50392f7
sZeta99 <the.black.mage.noir@gmail.com>
2023-03-02 21:40:51 +0100

    Refactoring Main Menu
    Ora ogni bottone ha il suo codice personale indipendente e riutilizzabile, manca resume game

--- GeneralTemplate/TemplateAssets/Scripts/UI/SettingMenu.gd
+++ GeneralTemplate/TemplateAssets/Scripts/UI/SettingMenu.gd
@@ -12,9 +12,9 @@ const RESOLUTION_SIZE: Dictionary = {0 : Vector2(720,576),
 
 #Probabilmente un bug ma gli index non sono ricalibrabili da fuori
 func _on_full_screen_button_item_selected(index):
-
 	DisplayServer.window_set_mode(DISPLAY_MODE.get(index))
 	print_debug("Full Screen mode set to " + str(DISPLAY_MODE.get(index)))
+	pass
 
 
 
@@ -22,4 +22,5 @@ func _on_resolution_button_item_selected(index):
 	print_debug(index)
 	DisplayServer.window_set_size(RESOLUTION_SIZE.get(index))
 	print_debug("Resolution Set to " + str(RESOLUTION_SIZE.get(index)))
-	
+	pass
+
