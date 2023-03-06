55fcbf3697c7ce8cbf7b480775e79ade969b371e
MauroZorzin <60018820+sZeta99@users.noreply.github.com>
2023-03-06 11:45:07 +0100

    Fix collision
    2 aree non collidono tra di loro

--- TemplateAssets/Scripts/CharacterController/collectorObject.gd
+++ TemplateAssets/Scripts/CharacterController/collectorObject.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+func _ready():
+	self.body_entered.connect(_on_body_entered)
+
 func _on_body_entered(body):
 	body.queue_free()
+	print_debug("Body freed")
 	pass # Replace with function body.
