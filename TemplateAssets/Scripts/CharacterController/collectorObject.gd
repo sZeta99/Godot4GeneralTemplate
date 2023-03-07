33bb35c39979a502ad90232ffa75fad7f3d61d77
fraking00 <75484827+fraking00@users.noreply.github.com>
2023-03-07 11:33:35 +0100

    signal inserted

--- TemplateAssets/Scripts/CharacterController/collectorObject.gd
+++ TemplateAssets/Scripts/CharacterController/collectorObject.gd
@@ -1,9 +1,12 @@
 extends Area2D
 
+signal coin_collected
+
 func _ready():
 	self.body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
+	emit_signal("coin_collected")
 	body.queue_free()
 	print_debug("Body freed")
 	pass # Replace with function body.
