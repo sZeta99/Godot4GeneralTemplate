164bf73d3a4e8319b617b9930a83a5217460b17c
MauroZorzin <60018820+sZeta99@users.noreply.github.com>
2023-03-23 22:00:17 +0100

    Base Interaction System
    Ci sono ancora cose da sistemare, ma voglio inazitutto fixare i packege

--- TemplateAssets/Scripts/CharacterController/collectorObject.gd
+++ TemplateAssets/Scripts/CharacterController/collectorObject.gd
@@ -2,16 +2,27 @@ extends Area2D
 class_name Collector 
 
 ## Groupe of the collectable Item, a Object mabey is better
-@export var groupe : String
+@export var group : String
 ## If false, the component will not function
 @export var active : bool = true
+
+## Check for group compatibility
+@export var groupeActive : bool = false
 
 func _ready():
 	self.body_entered.connect(_on_body_entered)
 	
 func _on_body_entered(body):
-	if body.is_in_group(groupe) and active:
-		SignalEventBusSingleton.emit_signal(SignalEventBusSingleton.ITEM_COLLECTED, body as Node2D)
-		body.queue_free()
-		print_debug("Item Collected")
-	pass # Replace with function body.
+	if active:
+		if groupeActive :
+			if body.is_in_group(group):
+				collect(body)
+		else :
+			collect(body)
+	pass
+
+func collect(body):
+	SignalEventBusSingleton.emit_signal(SignalEventBusSingleton.ITEM_COLLECTED, body as Node2D)
+	body.queue_free()
+	print_debug("Item Collected")
+	
--- TemplateAssets/Singleton/SignalEventBusSingleton.gd
+++ TemplateAssets/Singleton/SignalEventBusSingleton.gd
@@ -5,10 +5,7 @@ extends Node
 
 ##Enum 
 const ITEM_COLLECTED = "item_collected"
-const INTERACTED_TO = "interacted_to"
 
-## Fired on Player Interaction
-signal interacted_to(entity)
 # Fired on Collision of CollectrNode
 signal item_collected(item: Node2D)
 
